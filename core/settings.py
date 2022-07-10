4c0244332d01686e4b33cbd0d02789aa80cd133e
mohammadreza <mohammadrezasolo69@gmail.com>
2022-07-10 13:19:42 +0430

    add STATIC_URL STATIC_ROOT MEDIA_ROOT MEDIA_URL STATICFILES_DIRS in setting and url

--- core/settings.py
+++ core/settings.py
@@ -96,7 +96,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'root')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media', 'root')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
