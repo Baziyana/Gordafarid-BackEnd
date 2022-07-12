b7e86cf56e236e66212bdccded40ac46a9bd7507
mohammadreza <mohammadrezasolo69@gmail.com>
2022-07-12 19:14:28 +0430

    writ admin Post

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from blog.models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "thumbnail_image", 'created_at', "updated_at", "status")
+    list_editable = ('status',)
+    list_filter = ('author', 'status')
+    search_fields = ('title',)
